532b7040333733ccd10a58ff7b49217820373324
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-04-10 14:52:35 -0300

    deploy 1st prototype of swapi

---
+++ src/Naves.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+
+const Naves = () => {
+    const [nave, setNave] = useState('')
+    const [info, setInfo] = useState('Info das naves')
+
+    const FunctionFetchNaves = async () => {
+        const response = await fetch('https://swapi.dev/api/starships/')
+        const responseBody = await response.json();
+        let nomeNaves = responseBody.results.map((resultado, key) => <button className="nave-btn" key={key} onClick={() => FunctionFetchNavesInfo(key)}>{resultado.name}</button> );
+        return setNave(nomeNaves); // Returns name of ships
+    }
+    const FunctionFetchNavesInfo = async (props) => {
+        const response = await fetch(`https://swapi.dev/api/starships/`)
+        const responseBody = await response.json();
+        let result = responseBody.results[props]
+        return setInfo(Object.entries(result).map((items) => <li>{items}</li>));
+    }
+        // let resultKeys = Object.keys(result).map((infoKey, key) => <li key={key}>{infoKey}:</li>);
+
+    
+    
+    return (
+        <div className="nav-bar">
+        <button className="nave-search" onClick={() => FunctionFetchNaves(nave)}>Clique para buscar as naves</button>
+        <p>{nave}</p>
+        <ul className="nave-info-ul">
+            {info}
+        </ul>
+        </div>
+    )
+}
+
+export default Naves
