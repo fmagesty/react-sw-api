d73fdbbfacee883e02999bf984ba94da5354b76b
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-04-12 14:45:43 -0300

    footer pls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Naves from './Naves';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Naves />
+      <Footer />
     </div>
   );
 }
