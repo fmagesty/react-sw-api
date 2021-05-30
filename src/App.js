1034c250b428446ee4d7e6137a8c8372a064b782
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-05-30 09:26:30 -0300

    routing fix

--- src/App.js
+++ src/App.js
@@ -14,31 +14,31 @@ function App() {
     <Router>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="/react-sw-api/">
-            <Link to="/react-sw-api/">Home</Link>
+          <Navbar.Brand href="/react-sw-api">
+            <Link to="/react-sw-api">Home</Link>
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/react-sw-api/ships">Ships</Link>
-            <Link to="/react-sw-api/characters">Characters</Link>
-            <Link to="/react-sw-api/planets">Planets</Link>
+            <Link to={process.env.PUBLIC_URL + "/ships"}>Ships</Link>
+            <Link to={process.env.PUBLIC_URL + "/characters"}>Characters</Link>
+            <Link to={process.env.PUBLIC_URL + "/planets"}>Planets</Link>
           </Nav>
         </Container>
       </Navbar>
       <div className="App">
         <Switch>
-          <Route exact path="/react-sw-api/">
+          <Route exact path="/react-sw-api">
             <Homepage />
             <Footer />
           </Route>
-          <Route path="/react-sw-api/ships">
+          <Route path={process.env.PUBLIC_URL + "/react-sw-api/ships"}>
             <Ships />
             <Footer />
           </Route>
-          <Route path="/react-sw-api/characters">
+          <Route path={process.env.PUBLIC_URL + "/react-sw-api/characters"}>
             <Characters />
             <Footer />
           </Route>
-          <Route path="/react-sw-api/planets">
+          <Route path={process.env.PUBLIC_URL + "/react-sw-api/planets">}
             <Planets />
             <Footer />
           </Route>
